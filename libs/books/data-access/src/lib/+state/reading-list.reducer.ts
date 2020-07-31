096694150620f8de8efd85340a6fc805312aa2cc
swathi <swathiashok12@gmail.com>
2020-07-31 14:03:50 +0000

    Added code review and fixed tests

--- libs/books/data-access/src/lib/+state/reading-list.reducer.ts
+++ libs/books/data-access/src/lib/+state/reading-list.reducer.ts
@@ -52,7 +52,15 @@ const readingListReducer = createReducer(
   ),
   on(ReadingListActions.removeFromReadingList, (state, action) =>
     readingListAdapter.removeOne(action.item.bookId, state)
-  )
+  ),
+  on(ReadingListActions.failedAddToReadingList, (state, action) =>
+    readingListAdapter.removeOne(action.book.id, state)
+  ),
+  on(ReadingListActions.failedRemoveFromReadingList, (state, action) => {
+    return {
+      ...state,
+    };
+  }),
 );
 
 export function reducer(state: State | undefined, action: Action) {
